18a29feb86909bf48f7059a0d3ec33d15014c416
Johnny Nguyen <63935881+droidecahedron@users.noreply.github.com>
2025-03-28 08:22:54 -0700

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief >1 Mbps testing w uart00 on nrf54l15 soc
+ */
+#include <zephyr/kernel.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/uart.h>
+
+
+#define SLEEP_TIME_MS 20
+#define RECEIVE_BUF_SIZE 8
+#define TRANSMIT_BUF_SIZE 8
+#define RECEIVE_TIMEOUT 100
+
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart00));
+static uint8_t rx_buf[RECEIVE_BUF_SIZE] = {0};
+static uint8_t tx_buf[TRANSMIT_BUF_SIZE] = {0x00, 0x0B, 0x0A, 0x0D, 0x0C, 0x00, 0x0D, 0x0E};
+static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
+{
+        switch (evt->type)
+        {
+        case UART_TX_DONE:
+                break;
+        case UART_TX_ABORTED:
+                break;
+        case UART_RX_RDY:
+                break;
+        case UART_RX_BUF_REQUEST:
+                break;
+        case UART_RX_BUF_RELEASED:
+                break;
+        case UART_RX_DISABLED:
+                
+                uart_rx_enable(dev, rx_buf, sizeof rx_buf, RECEIVE_TIMEOUT);
+                break;
+        case UART_RX_STOPPED:
+                break;
+        default:
+                break;
+        }
+}
+
+int main(void)
+{
+        int ret = 0;
+        if (!device_is_ready(uart))
+        {
+                return 1;
+        }
+        ret = uart_callback_set(uart, uart_cb, NULL);
+        if (ret)
+        {
+                return 2;
+        }
+        
+        
+        while(1)
+        {
+                ret = uart_tx(uart, tx_buf, sizeof(tx_buf), SYS_FOREVER_MS);
+                k_msleep(SLEEP_TIME_MS);
+        }
+        return 0;
+}
